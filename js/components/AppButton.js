5a548874ade4de641b89dc19c59c3582a2c77c9b
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2025-03-06 21:14:10 +0500

    initial commit

---
+++ js/components/AppButton.js
@@ -0,0 +1,25 @@
+export default {
+    template:`
+            <button :class="{
+                'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+                'bg-blue-200 hover:bg-blue-400': type === 'primary',
+                'bg-red-200 hover:bg-red-400': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                'is-loading':processing
+            }"
+            :disabled="processing">
+                <slot />
+            </button>
+        `,
+
+        props: {
+            type: {
+                type: String,
+                default: 'secondary'
+            },
+        processing: {
+            type: Boolean,
+            default: false
+            }
+        },
+    }
