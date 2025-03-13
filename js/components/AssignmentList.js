fab07777df1a8e5ce559bb1c15a79ffc76ac95d3
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2025-03-13 20:29:33 +0500

    panel component added

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,10 +1,11 @@
 import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
+import Panel from "./Panel.js";
 export default {
-    components: {Assignment, AssignmentTags},
+    components: { Assignment, AssignmentTags, Panel },
     template: `
-            <section v-show="assignments.length" class="w-60">
-            <div class="flex justify-between item-start">
+            <panel v-show="assignments.length" >
+            <div class="flex justify-between items-start">
                 <h2 class="font-bold mb-2">
                     {{ title }}
                     <span>({{ assignments.length }})</span>
@@ -26,7 +27,12 @@ export default {
             </ul>
             
             <slot></slot>
-        </section>
+            
+            <template #footer>
+                This is footer section
+            </template>     
+            
+        </panel>
 
     `,
 
